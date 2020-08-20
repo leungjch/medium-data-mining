ea57b9302197212ed5a1590c128d7c5e148ddfa6
Justin Leung <justin.leung.ch@gmail.com>
2020-08-20 16:26:40 -0400

    add SOTorrent sql queries

--- sotorrent_bigquery/getTopDomains_Comments.sql
+++ sotorrent_bigquery/getTopDomains_Comments.sql
@@ -1,8 +1,8 @@
 #standardSQL
 SELECT RootDomain, COUNT(*) as freq
-FROM `sotorrent-org.2020_03_15.PostVersionUrl`
+FROM `sotorrent-org.2020_03_15.CommentUrl`
 GROUP BY RootDomain
 ORDER BY freq desc
 
--- Medium is the 70th most common domain linked
--- (44,489 occurrences)
+-- Medium is the 52th most common domain linked in comments
+-- (12,135 occurrences)
