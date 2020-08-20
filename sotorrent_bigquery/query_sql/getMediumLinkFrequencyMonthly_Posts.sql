f62d81ebada9ee72db0736141b67c620ce9a0d29
Justin Leung <justin.leung.ch@gmail.com>
2020-08-20 19:09:01 -0400

    cleanup bigquery folder structure

---
+++ sotorrent_bigquery/query_sql/getMediumLinkFrequencyMonthly_Posts.sql
@@ -0,0 +1,31 @@
+-- Posts
+-- Get frequency link references to Medium and Medium publications per month 
+-- Shows Medium's explosive growth in the past years
+#standardSQL
+SELECT
+  DATE_TRUNC(DATE(CreationDate), MONTH) as date,
+  COUNT(*) as count
+FROM sotorrent-org.2020_03_15.Posts
+INNER JOIN sotorrent-org.2020_03_15.PostVersionUrl
+ON Posts.ParentId = PostVersionUrl.PostId
+WHERE RootDomain LIKE '%medium.com%' 
+   OR RootDomain LIKE '%towardsdatascience.com%'
+   OR RootDomain LIKE '%codeburst.io%'
+GROUP BY date
+ORDER BY date ASC
+
+-- Comments
+-- Get frequency link references to Medium and Medium publications per month 
+-- Shows Medium's explosive growth in the past years\
+#standardSQL
+SELECT
+  DATE_TRUNC(DATE(CreationDate), MONTH) as date,
+  COUNT(*) as count
+FROM sotorrent-org.2020_03_15.Comments
+INNER JOIN sotorrent-org.2020_03_15.CommentUrl
+ON Comments.Id = CommentUrl.CommentId
+WHERE RootDomain LIKE '%medium.com%' 
+   OR RootDomain LIKE '%towardsdatascience.com%'
+   OR RootDomain LIKE '%codeburst.io%'
+GROUP BY date
+ORDER BY date ASC
