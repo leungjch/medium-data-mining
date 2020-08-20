ba8476839d2be398bba8980b1d019f0b7488af75
Justin Leung <justin.leung.ch@gmail.com>
2020-08-20 19:13:25 -0400

    cleanup bigquery folder structure

---
+++ sotorrent_bigquery/getNumberMediumLinks.sql
@@ -0,0 +1,17 @@
+-- Num. Medium articles in Posts
+--  0.13% (50735/37554088)
+#standardSQL
+SELECT COUNT(*)
+FROM `sotorrent-org.2020_03_15.PostVersionUrl`
+WHERE Url LIKE '%medium.com%' 
+   OR Url LIKE '%towardsdatascience.com%'
+   OR Url LIKE '%codeburst.io%';
+
+-- Num. Medium articles in Comments
+-- (0.17%) 13737/8139159
+#standardSQL
+SELECT COUNT(*)
+FROM `sotorrent-org.2020_03_15.CommentUrl`
+WHERE Url LIKE '%medium.com%' 
+   OR Url LIKE '%towardsdatascience.com%'
+   OR Url LIKE '%codeburst.io%';
